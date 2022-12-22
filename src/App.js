0e77d4db33af353c1d26186065fc57518350faa4
ePoths <epoth@naver.com>
2022-12-22 22:57:46 +0900

    Master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Home from "./routes/Home";
-import Detail from "./components/Detail";
+// import Detail from "./components/Detail";
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="movie" element={<Detail />} />
+      {/* <Route path="movie" element={<Detail />} /> */}
     </Routes>
   );
 }
